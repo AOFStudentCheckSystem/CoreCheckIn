901b8eda70d8cbfc398a622779b0d900f43f2c00
DEDZTBH <peiqial@outlook.com>
2017-09-30 11:13:13 -0400

    init

---
+++ src/main/kotlin/cn/com/guardiantech/corecheckin/CoreCheckinApplication.kt
@@ -0,0 +1,11 @@
+package cn.com.guardiantech.corecheckin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CoreCheckinApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(CoreCheckinApplication::class.java, *args)
+}
